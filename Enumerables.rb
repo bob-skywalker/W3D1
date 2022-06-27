bed4f1db1b4d36cc00011d17b5e26f29a91e2151
MohamedShneshn <98075987+mohamedshneshn@users.noreply.github.com>
2022-06-27 16:11:50 -0700

    2nd task

--- Enumerables.rb
+++ Enumerables.rb
@@ -5,4 +5,13 @@ class Array
         end
     self
     end
+    def my_select(&prc)
+         newArr=[]
+         self.my_each do |num|
+            if prc.call(num) == true
+                newArr << num
+            end
+         end  
+         newArr
+    end
 end
